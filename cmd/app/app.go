cc57d1bb1e5f1a4987b78623280df6826b50816a
ashtishad <ashefgo@gmail.com>
2023-11-04 14:04:28 +0600

    feat(handler): Server setup with gin. * SanityCheck checks for environment variables, if not set, it will set default values. * using structured logging using standard library slog, added it's configurations.

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,1 +1,48 @@
 package app
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ashtishad/fileverse/internal/domain"
+	"github.com/ashtishad/fileverse/internal/infra/database"
+	"github.com/ashtishad/fileverse/internal/service"
+	"github.com/ashtishad/fileverse/pkg/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	gin.SetMode(gin.ReleaseMode)
+
+	logger := utils.InitSlogger()
+
+	utils.SanityCheck(logger)
+
+	dbClient := database.GetDBClient(logger)
+
+	fileRepositoryDB := domain.NewFileRepoDB(dbClient, logger)
+	fileService := service.NewFileService(fileRepositoryDB, logger)
+
+	router := gin.Default()
+
+	fileHandlers := FileHandlers{s: fileService}
+
+	router.POST("/upload", fileHandlers.SaveFileHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("%s:%s", os.Getenv("API_HOST"), os.Getenv("API_PORT")),
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	logger.Info("Starting server " + srv.Addr)
+
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		logger.Error("could not start server", "err", err)
+	}
+}
